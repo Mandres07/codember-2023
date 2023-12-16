9a587e82f7113ffdde7b65af89e1f61de27b26f9
Mario Morales <mandres0807@outlook.com>
2023-12-15 22:03:58 -0500

    solved adventjs day 15

---
+++ adventjs/15/index.js
@@ -0,0 +1,53 @@
+function autonomousDrive(store, movements) {
+    const row = store.findIndex(s => s.includes('!'));
+    const column = store[row].indexOf('!');
+    const position = { row, column };
+    const updatedRow = store[row].replace('!', '.');
+    store[row] = updatedRow;
+    for (const move of movements) {
+        if (move === 'R') {
+            const ev = store[position.row][position.column + 1];
+            if (ev != undefined && ev === '.') {
+                position.column = position.column + 1;
+            }
+        }
+        else if (move === 'L') {
+            const ev = store[position.row][position.column - 1];
+            if (ev != undefined && ev === '.') {
+                position.column = position.column - 1;
+            }
+        }
+        else if (move === 'U') {
+            const ev = store[position.row - 1];
+            if (ev != undefined && ev[position.column] === '.') {
+                position.row = position.row - 1;
+            }
+        }
+        else {
+            const ev = store[position.row + 1];
+            if (ev != undefined && ev[position.column] === '.') {
+                position.row = position.row + 1;
+            }
+        }
+    }
+    const temp = store[position.row].split('');
+    temp[position.column] = '!';
+    store[position.row] = temp.join('');
+    return store;
+}
+const s = ['..!....', '...*.*.'];
+const m = ['R', 'R', 'D', 'L'];
+const res1 = autonomousDrive(s, m);
+console.log(res1);
+console.log(autonomousDrive([
+    '..!....',
+    '......*'
+], ['R', 'D', 'L']));
+console.log(autonomousDrive(['..!....'], ['R', 'L']));
+/*
+[
+  ".......",
+  "...*!*."
+]
+*/
+// The last movement is to the left, but it cannot move because there is an obstacle.
